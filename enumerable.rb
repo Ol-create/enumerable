c6a6f0b988d6980e96784dc1fafb379fa6912015
Oluyemi Paul <oluola96@gmail,com>
2022-10-29 14:47:31 +0100

    Resolve linters err

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,23 @@
 module MyEnumerable
-    def all?
-      each do |e|
-        return false unless yield(e)
-      end
-      true
+  def all?
+    each do |e|
+      return false unless yield(e)
     end
-  
-    def any?
-      each do |e|
-        return true if yield(e)
-      end
-      false
-    end
-  
-    def filter
-      filtered = []
-      each do |e|
-        filtered.push(e) if yield(e)
-      end
-      filtered
-    end
+    true
   end
+  
+  def any?
+    each do |e|
+      return true if yield(e)
+    end
+    false
+  end
+  
+  def filter
+    filtered = []
+    each do |e|
+      filtered.push(e) if yield(e)
+    end
+    filtered
+  end
+end
